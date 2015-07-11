46f6b0fd0abc8a62d775030d5fcb13d8fa55e5f3
stevievines <stevievines@gmail.com>
2015-07-11 11:57:02 -0400

    Add basic secrets view for devise auth proof of concept

--- rails-bootstrap-devise/config/routes.rb
+++ rails-bootstrap-devise/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'visitors#index'
 
+  resources 'secrets', only: :index
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
